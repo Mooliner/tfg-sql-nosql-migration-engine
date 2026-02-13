9d032d163dc26cb2a474bd4fc20e2d9c16a48c0b
Gerard Moliner <gerymoliner@gmail.com>
2026-02-13 19:16:27 +0100

    Configuracio macOS

---
+++ app/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+
+    model_config = {"from_attributes": True}
