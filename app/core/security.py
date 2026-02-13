4a8978f2fcbf191f3676dec6ff959284c0a0f388
Gerard Moliner <gerymoliner@gmail.com>
2026-02-13 19:15:47 +0100

    Netetja de gitignore i configuració macOS

--- app/core/security.py
+++
@@ -1,7 +0,0 @@
-import logging
-from passlib.context import CryptContext
-
-# Això silencia l'error de lectura de versió de bcrypt que veus al log
-logging.getLogger("passlib").setLevel(logging.ERROR)
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
